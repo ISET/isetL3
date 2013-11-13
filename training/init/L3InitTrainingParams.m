e4cc926a16ec3a61a7f60b39fe2caf1912e96438
Steven Lansel <steve.lansel@gmail.com>
2013-11-12 18:39:48 -0800

    Moved to default the training parameters for bias/variance tradeoff

--- training/init/L3InitTrainingParams.m
+++ training/init/L3InitTrainingParams.m
@@ -34,6 +34,17 @@ patchLuminanceSamples = [0.001, 0.0016, 0.0026, 0.0041, 0.0065, 0.0104, 0.0166, 
                            0.4505, 0.6753, 0.9, 1.1248, 1.3495, 1.5743, 0.99*voltageswing];
 L3 = L3Set(L3,'luminance list', patchLuminanceSamples);
 
+%% Bias and Variance Weights
+weights = [4, 4, 4];  
+L3 = L3Set(L3, 'global weight bias variance', weights);
+
+weights = [4, 16, 4];
+L3 = L3Set(L3, 'flat weight bias variance', weights);
+
+weights = [4, 1, 4];
+L3 = L3Set(L3, 'texture weight bias variance', weights);
+
+
 %% Defaults that were previously in L3Create
 L3 = L3Set(L3,'n oversample',0);
 L3 = L3Set(L3,'saturation flag', 1);
