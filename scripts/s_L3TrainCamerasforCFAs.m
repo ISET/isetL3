ed21db5cb1c715f703e34ccd5aeca4e8185b83f0
Steven Lansel <steve.lansel@gmail.com>
2013-11-24 08:19:51 -0800

    Finished updating all camera files

--- scripts/s_L3TrainCamerasforCFAs.m
+++ scripts/s_L3TrainCamerasforCFAs.m
@@ -24,8 +24,7 @@ if exist(saveFolder, 'dir')~=7
 end
 
 %% Train Camera for each CFA 
-% for cfaFilenum = 1:length(cfaFiles)
-for cfaFilenum = 29
+for cfaFilenum = 1:length(cfaFiles)
     cfaFile = cfaFiles(cfaFilenum).name;
     disp(['CFA:  ', cfaFile, '  ', num2str(cfaFilenum),' / ', num2str(length(cfaFiles))])    
     camera = L3TrainCameraforCFA(cfaFile);
