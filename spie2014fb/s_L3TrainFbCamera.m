30f3482266dbb5dfee7b1cf799dc4798d51beb88
Qiyuan Tian <fdqiyuantian@gmail.com>
2014-07-22 10:37:47 -0700

    Added back the spie2014fb folder that is lost when I merged the master branch to the spie2014fb branch

---
+++ spie2014fb/s_L3TrainFbCamera.m
@@ -0,0 +1,31 @@
+clear, clc, close all
+
+%% Start ISET
+s_initISET
+
+%% Create and initialize L3 structure
+L3 = L3Create;
+wave = [400:10:680]';
+[sensor, optics] = fbCreate(wave');
+
+oi = oiCreate;
+oi = oiSet(oi,'optics',optics);
+oi = oiSet(oi,'name','fb optics');
+
+L3 = L3Initialize(L3, [], oi, sensor, []); 
+
+%% Change luminance list
+sensorD = L3Get(L3, 'design sensor');
+volswing = sensorGet(sensorD, 'voltage swing');
+lumlist = linspace(0.01, 0.99*volswing, 30);
+L3 = L3Set(L3, 'luminance list', lumlist);
+
+%% Perform training
+L3 = L3Train(L3);
+
+%% Setup L3 camera
+camera = L3CameraCreate(L3);
+
+%% Save data
+save('L3_fb_30', 'L3');
+save('L3camera_fb_30', 'camera');
