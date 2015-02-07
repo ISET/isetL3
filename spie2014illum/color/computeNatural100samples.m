69e0a30df6fa16bfca1db6a822d9df87df635235
Francois Germain <fgermain@stanford.edu>
2015-02-07 15:48:06 -0800

    New PCA figures, eps and png

--- spie2014illum/color/computeNatural100samples.m
+++ spie2014illum/color/computeNatural100samples.m
@@ -27,7 +27,12 @@ if plotFlag
    figure, set(gcf,'Position',[0 0 800 400])
      plot(wavelength,U,'LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
     legend('Component 1','Component 2','Component 3','Component 4','Component 5','Component 6','Component 7','Location','northeastoutside')
-    export_fig('PCA.eps','-eps','-transparent');   
+    export_fig('PCA.eps','-eps');   
+    export_fig('PCA.png','-png');   
+     plot(wavelength,dataMean,'k--','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b'), ylim([0 Inf])
+    legend('PCA Mean','Location','northeastoutside')
+    export_fig('PCAm.eps','-eps');   
+    export_fig('PCAm.png','-png');   
 end
 
 % Clothes reflectances
@@ -48,12 +53,14 @@ for i = 1:N
 end
 
 if plotFlag
-    plot(wavelength,r(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
+    plot(wavelength,r(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b'), ylim([0 Inf])
     legend('Clothes 1','Clothes 2','Clothes 3','Clothes 4','Clothes 5','Clothes 6','Clothes 7','Location','northeastoutside')
-    export_fig('ClothesR.eps','-eps','-transparent');
+    export_fig('ClothesR.eps','-eps');
+    export_fig('ClothesR.png','-png');
     plot(1:7,cPCA(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
     legend('Clothes 1','Clothes 2','Clothes 3','Clothes 4','Clothes 5','Clothes 6','Clothes 7','Location','northeastoutside')
-    export_fig('ClothesP.eps','-eps','-transparent');
+    export_fig('ClothesP.eps','-eps');
+    export_fig('ClothesP.png','-png');
 end
     
 
@@ -70,21 +77,25 @@ dataM = bsxfun(@minus,data,dataMean);
 N = 20;
 r = zeros(N,length(wavelength));
 c = P * dataM;
+cPCA = zeros(N,7);
 Z = linkage(c');
 T = cluster(Z,'maxclust',N);
 for i = 1:N
     k = find(T == i);
     k = k(randperm(length(k),1));
     r(i,:) = data(:,k);
+    cPCA(i,:) = c(:,k);
 end
 
 if plotFlag
-    plot(wavelength,r(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
+    plot(wavelength,r(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b'), ylim([0 Inf])
     legend('Food 1','Food 2','Food 3','Food 4','Food 5','Food 6','Food 7','Location','northeastoutside')
-    export_fig('FoodR.eps','-eps','-transparent');
+    export_fig('FoodR.eps','-eps');
+    export_fig('FoodR.png','-png');
     plot(1:7,cPCA(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
     legend('Food 1','Food 2','Food 3','Food 4','Food 5','Food 6','Food 7','Location','northeastoutside')
-    export_fig('FoodP.eps','-eps','-transparent');
+    export_fig('FoodP.eps','-eps');
+    export_fig('FoodP.png','-png');
 end
 
 XYZ = ieXYZFromPhotons([r;ones(1,length(wavelength))],wavelength);
@@ -103,21 +114,25 @@ dataM = bsxfun(@minus,data,dataMean);
 N = 20;
 r = zeros(N,length(wavelength));
 c = P * dataM;
+cPCA = zeros(N,7);
 Z = linkage(c');
 T = cluster(Z,'maxclust',N);
 for i = 1:N
     k = find(T == i);
     k = k(randperm(length(k),1));
     r(i,:) = data(:,k);
+    cPCA(i,:) = c(:,k);
 end
 
 if plotFlag
-    plot(wavelength,r(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
+    plot(wavelength,r(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b'), ylim([0 Inf])
     legend('Nature 1','Nature 2','Nature 3','Nature 4','Nature 5','Nature 6','Nature 7','Location','northeastoutside')
-    export_fig('NatureR.eps','-eps','-transparent');
+    export_fig('NatureR.eps','-eps');
+    export_fig('NatureR.png','-png');
     plot(1:7,cPCA(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
     legend('Nature 1','Nature 2','Nature 3','Nature 4','Nature 5','Nature 6','Nature 7','Location','northeastoutside')
-    export_fig('NatureP.eps','-eps','-transparent');
+    export_fig('NatureP.eps','-eps');
+    export_fig('NatureP.png','-png');
 end
 
 XYZ = ieXYZFromPhotons([r;ones(1,length(wavelength))],wavelength);
@@ -133,21 +148,25 @@ dataM = bsxfun(@minus,data,dataMean);
 N = 5;
 r = zeros(N,length(wavelength));
 c = P * dataM;
+cPCA = zeros(N,7);
 Z = linkage(c');
 T = cluster(Z,'maxclust',N);
 for i = 1:N
     k = find(T == i);
     k = k(randperm(length(k),1));
     r(i,:) = data(:,k);
+    cPCA(i,:) = c(:,k);
 end
 
 if plotFlag
-    plot(wavelength,r(1:5,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
+    plot(wavelength,r(1:5,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b'), ylim([0 Inf])
     legend('Hair 1','Hair 2','Hair 3','Hair 4','Hair 5','Location','northeastoutside')
-    export_fig('ClothesR.eps','-eps','-transparent');
+    export_fig('HairR.eps','-eps');
+    export_fig('HairR.png','-png');
     plot(1:7,cPCA(1:5,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
     legend('Hair 1','Hair 2','Hair 3','Hair 4','Hair 5','Location','northeastoutside')
-    export_fig('HairP.eps','-eps','-transparent');
+    export_fig('HairP.eps','-eps');
+    export_fig('HairP.png','-png');
 end
 
 XYZ = ieXYZFromPhotons([r;ones(1,length(wavelength))],wavelength);
@@ -163,21 +182,25 @@ dataM = bsxfun(@minus,data,dataMean);
 N = 15;
 r = zeros(N,length(wavelength));
 c = P * dataM;
+cPCA = zeros(N,7);
 Z = linkage(c');
 T = cluster(Z,'maxclust',N);
 for i = 1:N
     k = find(T == i);
     k = k(randperm(length(k),1));
     r(i,:) = data(:,k);
+    cPCA(i,:) = c(:,k);
 end
 
 if plotFlag
-    plot(wavelength,r(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
+    plot(wavelength,r(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b'), ylim([0 Inf])
     legend('Skin 1','Skin 2','Skin 3','Skin 4','Skin 5','Skin 6','Skin 7','Location','northeastoutside')
-    export_fig('SkinR.eps','-eps','-transparent');
+    export_fig('SkinR.eps','-eps');
+    export_fig('SkinR.png','-png');
     plot(1:7,cPCA(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
     legend('Skin 1','Skin 2','Skin 3','Skin 4','Skin 5','Skin 6','Skin 7','Location','northeastoutside')
-    export_fig('SkinP.eps','-eps','-transparent');
+    export_fig('SkinP.eps','-eps');
+    export_fig('SkinP.png','-png');
 end
 
 XYZ = ieXYZFromPhotons([r;ones(1,length(wavelength))],wavelength);
@@ -193,21 +216,25 @@ dataM = bsxfun(@minus,data,dataMean);
 N = 20;
 r = zeros(N,length(wavelength));
 c = P * dataM;
+cPCA = zeros(N,7);
 Z = linkage(c');
 T = cluster(Z,'maxclust',N);
 for i = 1:N
     k = find(T == i);
     k = k(randperm(length(k),1));
     r(i,:) = data(:,k);
+    cPCA(i,:) = c(:,k);
 end
 
 if plotFlag
-    plot(wavelength,r(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
+    plot(wavelength,r(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b'), ylim([0 Inf])
     legend('Dupont 1','Dupont 2','Dupont 3','Dupont 4','Dupont 5','Dupont 6','Dupont 7','Location','northeastoutside')
-    export_fig('DupontR.eps','-eps','-transparent');
+    export_fig('DupontR.eps','-eps');
+    export_fig('DupontR.png','-png');
     plot(1:7,cPCA(1:7,:)','LineWidth',2), axis tight, set(gca,'FontSize',13,'FontWeight','b')
     legend('Dupont 1','Dupont 2','Dupont 3','Dupont 4','Dupont 5','Dupont 6','Dupont 7','Location','northeastoutside')
-    export_fig('DupontP.eps','-eps','-transparent');
+    export_fig('DupontP.eps','-eps');
+    export_fig('DupontP.png','-png');
 end
 
 XYZ = ieXYZFromPhotons([r;ones(1,length(wavelength))],wavelength);
