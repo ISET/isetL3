e12314e3fb811b7289f0981a2eaa1b60ca6e8468
Brian Wandell <wandell@stanford.edu>
2018-10-05 18:13:47 -0700

    Comments

--- scripts/quadsensor/sensorCreateQuad.m
+++ scripts/quadsensor/sensorCreateQuad.m
@@ -1,13 +1,25 @@
 function sensor = sensorCreateQuad
 % Create a quad sensor Bayer filter
 %
-% https://www.gsmarena.com/explaining_huawei_p20_pros_triple_camera-news-30497.php
+% Syntax
+%    sensor = sensorCreateQuad(varargin)
 %
-% Wandell
+% Brief Description:
+%
+%   https://www.gsmarena.com/explaining_huawei_p20_pros_triple_camera-news-30497.php
+%
+%   We are experimenting with this type of sensor.  In the near
+%   feature we will have optical images created that account for the
+%   microlens properties in front of the sensor also.
+%
+% See also:
+%   L3 related functions should all work with this.
+%
 
-%%
+%% Start with a basic sensor
 sensor = sensorCreate;
 
+%% Adjust its properties to the quad format
 quadPattern = ...
     [3 3 2 2; ...
     3 3 2 2; ...
@@ -15,6 +27,9 @@ quadPattern = ...
     2 2 1 1];
 
 sensor = sensorSet(sensor,'pattern',quadPattern);
+
+%% Set up the other parameters - To be done
+
 % sensor = sensorSetSizeToFOV(sensor,sceneGet(scene,'fov'));
 sensor = sensorSet(sensor,'pixel size constant fill factor',1.4e-6);
 % sensor = sensorCompute(sensor,oi);
