2092dc58a1330345e14b2cfbffce836bdd6edb7e
Zheng Lyu <zhenglyu@vistalab-compute-1.c.som-vistalab.internal>
2018-12-19 15:58:40 -0800

    Cleaned the tutorials for L3 in ML branch

--- tutorials/t_L3ClassifyWithCfa.m
+++
@@ -1,3 +0,0 @@
-% Note for how to modify the l3ClassifyFast to make it possible to classify
-% according to the cfa. The current plan is to change the indx
-
--- tutorials/t_L3SuperResolution.m
+++
@@ -1,3 +0,0 @@
-%% t_L3SuperResolution
-% This is an experiment that we explore the super resolution based on l3
-% approach.
