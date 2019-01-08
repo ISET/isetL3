0484477663f39db8842e7ee43ddae76261eb8794
Brian Wandell <wandell@stanford.edu>
2019-01-07 17:44:26 -0800

    Comments, a little neatening

--- utility/loadScenes.m
+++ utility/loadScenes.m
@@ -1,22 +1,29 @@
 function scenes = loadScenes(scenePath, format, idxScene, varargin)
-%% loadScenes
+% Load selected scenes
+%
+%
+%
+% ZhengLyu, SCIEN Team, 2018
 
-%% 
+%
+%{
+ scenePath = fullfile(huaquadrootpath,'local','scenes')
+ format = 'mat';
+ loadScenes(scenePath,format,[1 2]);
+%}
+%%
 
 fileName = strcat('*.', format);
 
+% filePath = fullfile(scenePath, fileName);
+filesToLoad = dir(fullfile(scenePath,fileName));
+nFiles = length(idxScene);
+scenes = cell(nFiles, 1);
 
-filePath = fullfile(scenePath, fileName);
-filesToLoad = dir(filePath);
-nFiles = length(filesToLoad);
-scenes = cell(length(idxScene), 1);
-
-for ii = 1 : length(idxScene)
-
-    sceneName = filesToLoad(idxScene(ii)).name;
+for ii = 1:nFiles
+    sceneName  = fullfile(scenePath,filesToLoad(idxScene(ii)).name);
     scenes{ii} = load(sceneName);
     scenes{ii} = sceneSet(scenes{ii}, 'fov', 10);
 end
-
 
 end
